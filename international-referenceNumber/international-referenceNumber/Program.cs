46f6116ddd248a8cd4fab83033822d2f40465b5b
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-02-21 08:35:31 +0200

    Adding international referenceNumber project to Ekoodi-Training repository

---
+++ international-referenceNumber/international-referenceNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace international_referenceNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReferenceNumberHandler test = new ReferenceNumberHandler();
+            Console.WriteLine(test.CalculateInternationalReferenceNumber(" 23 48 236 "));
+            Console.WriteLine(test.CalculateInternationalReferenceNumber(" 123456 ",true));
+            Console.WriteLine(test.CheckReferenceNumber("Rf 97 123 456 "));
+
+            Console.ReadKey();
+
+        }
+    }
+}
