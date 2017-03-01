bb981a96bb8dbf7f01dc2b100850ca38fb3d45c8
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-03-01 09:29:16 +0200

    AspNet Start whit Asp.NET practices

---
+++ AspNet/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //// GET: HelloWorld
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index() //http://localhost:xxx/HelloWorld
+        {
+            return "This is my <b>default</b> action...<br>Terveisin Mikko :)";
+        }
+
+        //public string Welcome() //http://localhost:xxx/HelloWorld/Welcome
+        //{
+        //    return "This is my Welcome action method...<br>Ei sillä ettenkö epäilisi... ;)";
+        //}
+
+        public string Welcome(string name, int id=1) //http://localhost:xxx/HelloWorld/Welcome/3?name=Rick
+        {
+            return HttpUtility.HtmlEncode("Terve " + name + ", ID: " + id);
+        }
+
+    }
+}
