bf7d6669a0798b4928a07f22a7919db80bb54015
mikkof1 <mikkolaitinenf1@hotmail.com>
2017-03-03 09:48:58 +0200

    AspNet New start with new instructions New guide page https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc

---
+++ AspNet/NetCoreTest/src/NetCoreTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
