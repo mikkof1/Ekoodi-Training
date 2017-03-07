6f0bcb1e51fa612e3e14b230dbc6c29111fe2ed8
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-03-07 13:38:30 +0200

    WebApi (more tests) Add: Multiple reference number maker api (paht: /multi) takes object {"FirstReferenceNumber":"123456","Count":10} and returns list<string> of reference numbers

---
+++ iisApi/WebApi/WebApi/Controllers/MultipleReferenceController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    public class MultipleReferenceController : ApiController
+    {
+        private readonly ReferenceService _referenceService = new ReferenceService();
+
+        [HttpPost]
+        [Route("multi")]
+        public List<string> Post(MultipleReferenceNumber id)
+        {
+            List<string> returnList = new List<string>();
+
+            int countIndex = 1;
+            for (int i = 0; i < id.Count; i++)
+            {
+                returnList.Add(_referenceService.MakeFinnishReferenceNumber(id.FirstReferenceNumber+countIndex));
+                countIndex++;
+            }
+
+            return returnList;
+        }
+
+
+    }
+}
---
+++ iisApi/WebApi/WebApi/Models/MultipleReferenceNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class MultipleReferenceNumber
+    {
+        public MultipleReferenceNumber(string firstReferenceNumber, int count)
+        {
+            FirstReferenceNumber = firstReferenceNumber;
+            Count = count;
+        }
+
+        public string FirstReferenceNumber { get; set; }
+        public int Count { get; set; }
+    }
+}
