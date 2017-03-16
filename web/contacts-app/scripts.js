a3b88d98d5907b96a9751e5b0b70ec5299ea3a17
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-03-16 23:02:59 +0200

    contacts-app changes + form + scripts    * read form    * make table + css    * first style

--- web/contacts-app/scripts.js
+++ web/contacts-app/scripts.js
@@ -1,3 +1,44 @@
 /**
  * Created by ekoodi on 16.3.2017.
  */
+var users = [];
+function onClicked() {
+    var newUser = createUser();
+    users.push(newUser)
+    console.log(newUser);
+    console.log('users count: '+users.length);
+    fillTable();
+}
+
+function createUser() {
+    return {
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        phone: document.getElementById('phone').value,
+        street: document.getElementById('street').value,
+        city: document.getElementById('city').value
+    };
+}
+
+
+function fillTable() {
+    var table = document.getElementById("resultsTable");
+    table.innerHTML="<thead><tr><th>First name</th><th>Last name</th><th>Phone</th><th>Address</th></tr></thead>";
+    for (var i = 0; i < users.length; i++) {
+
+        var row = table.insertRow(table.rows.length);
+        var fName = row.insertCell(0);
+        var lName = row.insertCell(1);
+        var pho = row.insertCell(2);
+        var adress = row.insertCell(3);
+
+        fName.innerText = users[i].firstName;
+        lName.innerText = users[i].lastName;
+        pho.innerText = users[i].phone;
+        adress.innerText = users[i].street + ' , ' + users[i].city;
+
+        if(i%2==0) {row.className="evenRow";}
+        else {row.className="oddRow";}
+    }
+
+}
