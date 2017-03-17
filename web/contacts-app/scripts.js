7c06eca094593b5ca3786e54df001178059d9565
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-03-17 13:39:09 +0200

    contacts-app so far add methods  + edit  + delete  + empty input fields
    css
     + Make page more beautifully

--- web/contacts-app/scripts.js
+++ web/contacts-app/scripts.js
@@ -1,16 +1,56 @@
 /**
  * Created by ekoodi on 16.3.2017.
  */
-var users = [];
-function onClicked() {
-    var newUser = createUser();
-    users.push(newUser)
-    console.log(newUser);
-    console.log('users count: '+users.length);
-    fillTable();
+
+var contacts = [];
+var editMode = -1;
+
+function onLoad() {
+    if (typeof (Storage) !== 'undefined') {
+      var  data = localStorage.getItem("data");
+        if (data == null) {
+            contacts = [];
+            return;
+        }
+        contacts=JSON.parse(data);
+        fillTable();
+    }
 }
 
-function createUser() {
+function onClicked() {
+    if (checkEmpyts()) {
+        return;
+    }
+    var newContact = createContact();
+    if (editMode > -1) {
+        editMode=-1;
+        contacts[editMode] = newContact;
+        document.getElementById("addButton").innerText = 'Add';
+    }
+    else {
+        contacts.push(newContact);
+    }
+    saveData();
+    fillTable();
+    emptyInputs();
+}
+
+function saveData() {
+    localStorage.removeItem("data");
+    localStorage.setItem("data",JSON.stringify( contacts));
+}
+
+function checkEmpyts() {
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == '') {
+            alert('Hellou some input fields are empty :) ');
+            return true;
+        }
+    }
+}
+
+function createContact() {
     return {
         firstName: document.getElementById('firstName').value,
         lastName: document.getElementById('lastName').value,
@@ -20,25 +60,59 @@ function createUser() {
     };
 }
 
-
 function fillTable() {
     var table = document.getElementById("resultsTable");
-    table.innerHTML="<thead><tr><th>First name</th><th>Last name</th><th>Phone</th><th>Address</th></tr></thead>";
-    for (var i = 0; i < users.length; i++) {
+    table.innerHTML = "<tr><th>First name</th><th>Last name</th><th>Phone</th><th>Address</th><th></th><th></th></tr>";
+    for (var i = 0; i < contacts.length; i++) {
 
         var row = table.insertRow(table.rows.length);
         var fName = row.insertCell(0);
         var lName = row.insertCell(1);
         var pho = row.insertCell(2);
         var adress = row.insertCell(3);
+        var edit = row.insertCell(4);
+        var del = row.insertCell(5);
 
-        fName.innerText = users[i].firstName;
-        lName.innerText = users[i].lastName;
-        pho.innerText = users[i].phone;
-        adress.innerText = users[i].street + ' , ' + users[i].city;
+        fName.innerText = contacts[i].firstName;
+        lName.innerText = contacts[i].lastName;
+        pho.innerText = contacts[i].phone;
+        adress.innerText = contacts[i].street + ' , ' + contacts[i].city;
+        edit.innerHTML = '<button class="btnEdit" onclick="onEdit(' + i + ')">Edit</button>';
+        del.innerHTML = '<button class="btnDelete" onclick="onDelete(' + i + ')">Delete</button>';
 
-        if(i%2==0) {row.className="evenRow";}
-        else {row.className="oddRow";}
+        if (i % 2 == 0) {
+            row.className = "evenRow";
+        }
+        else {
+            row.className = "oddRow";
+        }
     }
+}
 
+function emptyInputs() {
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].value = '';
+    }
+}
+
+function onEdit(editIndex) {
+    document.getElementById("firstName").value = contacts[editIndex].firstName;
+    document.getElementById("lastName").value = contacts[editIndex].lastName;
+    document.getElementById("phone").value = contacts[editIndex].phone;
+    document.getElementById("street").value = contacts[editIndex].street;
+    document.getElementById("city").value = contacts[editIndex].city;
+    document.getElementById("addButton").innerText = 'Edit';
+    editMode = editIndex;
+}
+
+function onDelete(delIndex) {
+    var editTable = [];
+    for (var i = 0; i < contacts.length; i++) {
+        if (i != delIndex) {
+            editTable.push(contacts[i]);
+        }
+    }
+    contacts = editTable;
+    fillTable();
 }
