84cdf4306befbf45ce1497a7e73ac65fd024d7a1
mikkof1 <mikkolaitinenf1@hotmail.com>
2017-03-29 00:11:23 +0300

    blocker-game add: * drawing evil and hero * movements whit keys

--- blocker-game/drawGame.js
+++ blocker-game/drawGame.js
@@ -2,40 +2,96 @@
  * Created by ekoodi on 28.3.2017.
  */
 gameApp.drawGame = function () {
-
-    var canvas = document.getElementById("gameCanvas");
-    var cHei=canvas.height;
-    var cWid=canvas.width;
+  // https://www.kirupa.com/html5/animating_many_things_on_a_canvas.htm
+   // var canvas = document.getElementById("gameCanvas");
     var ctx = canvas.getContext("2d");
-
+//	var ctxHero = canvas.getContext("2d");
+//	 var ctxEvil = canvas.getContext("2d");
+	//var evilWallTimer;
+	//var heroTimer;
+	var gameOn=true;
+	var rounds=3;
+	var evilSpeed=3;
+	var backColor="#ffe6cc";
 
 
     function drawGame() {
-        setInterval(moveEvil,100);
-        function moveEvil() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            if(evil1.x>(-1*evil1.width)+3) {
-                evil1.x -= 8;
-                ctx.fillStyle = evil1.color;
-                ctx.fillRect(evil1.x, evil1.y, evil1.width, evil1.height);
-                evil2.x -= 8;
-                ctx.fillStyle = evil2.color;
-                ctx.fillRect(evil2.x, evil2.y, evil2.width, evil2.height);
-            }
-
-            ctx.fillStyle = player.color;
-            ctx.fillRect(player.x, player.y, player.width, player.height);
-        }
-
-
-
+		
+		setPlayer();
+		 newEvilWall();
+  //  evilWallTimer = setInterval(draw,20);
+	// heroTimer=setInterval(moveHero,10);
+		// drawHero();	
+		draw();
 
     }
+	
+	function draw(){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle = backColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+		evilUpdate();
+		heroUpdate();
+		if(gameOn){
+		requestAnimationFrame(draw);
+		}
+	}
+	/*
+	function drawHero(){
+		ctxHero.clearRect(0, 0, canvas.width, canvas.height);
+
+		heroUpdate();
+		
+		requestAnimationFrame(drawHero);
+		
+	}*/
+	
+	function evilUpdate() {
+          //  ctx.clearRect(0, 0, canvas.width, canvas.height);
+            evil1.x -= evilSpeed;
+            ctx.fillStyle = evil1.color;
+            ctx.fillRect(evil1.x, evil1.y, evil1.width, evil1.height);
+            evil2.x -= evilSpeed;
+            ctx.fillStyle = backColor;
+            ctx.fillRect(evil2.x, evil2.y, evil2.width, evil2.height);
+			
+			var stopTheEvilWall=evil1.x< -1*evil1.width;			
+            if(stopTheEvilWall) {
+			//clearInterval(evilWallTimer);
+				newEvilWall();
+				rounds--;
+				if(rounds<1){
+					gameOn=false;
+				}
+			}
+
+
+        }
+		
+	function heroUpdate(){
+		
+		ctx.fillStyle = player.color;
+        ctx.fillRect(player.x, player.y, player.width, player.height);
+		
+	}
+	
+	function newEvilWall(){
+		var cHei=canvas.height;
+        var cWid=canvas.width;
+		var place = Math.floor((Math.random() * cHei-160-40)+80+40);
+		 evil1 = gameApp.player(wid-30, 0, 30, cHei, "red");
+		 evil2 = gameApp.player(wid-30, place, 30, 80, "white");
+	}
+	
+	function setPlayer(){
+		 player = gameApp.player(30, hei / 2, 50, 50, "violet");
+	}
 
     return {
         draw:function () {
             drawGame();
-        }
+        },
+		
 
     }
 }
--- blocker-game/playGame.js
+++ blocker-game/playGame.js
@@ -2,36 +2,59 @@
  * Created by ekoodi on 28.3.2017.
  */
 gameApp.playGame = (function () {
+var move=10;
 
+ var heroTimer;
+ 
 
     function startNew() {
 
+	
         gameApp.drawGame().draw();
-        gameApp.drawGame().drawPlayer(player);
     }
 
     function goUpper() {
-        player.y = player.y - 10;
-        gameApp.drawGame().draw();
+//heroTimer=setInterval(uppp,100);
+//function uppp(){
+	if(player.y>0)
+        player.y = player.y - move;
+//}
+		//requestAnimationFrame(goUpper);
+      //  gameApp.drawGame.draw();
     }
 
     function goDowner() {
-        player.y = player.y + 10;
-        gameApp.drawGame().draw();
+		if(player.y<canvas.height-player.height)
+        player.y = player.y + move;
+       // gameApp.drawGame.draw();
     }
 
     return {
         onStart: function () {
             startNew();
+			document.getElementById("points").innerText="Nyt alkaaa";
         },
-        goUp: function () {
-         //   setInterval(goUpper(), 100);
-            goUpper();
-
+        goUpUp: function () {
+         
+            goUpper();	
         },
+		goUpDown: function(){
+			//player.y = player.y;
+			clearInterval(heroTimer);
+		},
         goDown: function () {
             goDowner();
-        }
+        },
+		doKeyDown:function(e){
+			document.getElementById("points").innerText="Textiä: "+e.keyCode;
+			if(e.keyCode==38 || e.keyCode== 87){ // up/w
+			goUpper();
+			}
+			
+			if(e.keyCode==40 || e.keyCode== 83){ // down/s
+			goDowner();
+			}
+		}
 
     }
 })();
